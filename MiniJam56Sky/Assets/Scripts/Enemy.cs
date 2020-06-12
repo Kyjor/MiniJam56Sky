251bfb7dad55f2f7d50178fd2c3aaac2e7219663
Alex DeCamillo <adecamillo@rockhall.org>
2020-06-12 18:09:57 -0400

    Enemies take damage

---
+++ MiniJam56Sky/Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health;
+
+    public void TakeDamage(float damage)
+    {
+        Debug.Log("took damge" + damage);
+        this.health -= damage;
+        if (health <= 0)
+        {
+            // die
+            GameObject.Destroy(this.gameObject, 0.001f);
+        }
+    }
+}
